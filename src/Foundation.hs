d32d719c4509d3c0e31a2cf56d5dcca975852bc9
giuliods <giuliodestasio98@gmail.com>
2018-10-10 16:50:03 +0200

    Riparazioni di Foundation.hs e modifica dei messaggi d'errore

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -25,6 +25,7 @@ import Yesod.Default.Util          (addStaticContentExternal)
 import qualified Yesod.Core.Unsafe as Unsafe
 import qualified Data.CaseInsensitive as CI
 import qualified Data.Text.Encoding as TE
+import Data.Text (pack)
 
 -- | The foundation datatype for your application. This can be a good place to
 -- keep settings and values requiring initialization before your application
